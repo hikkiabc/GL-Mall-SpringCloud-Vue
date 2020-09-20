a84e26cbe0e9f1b864550dbc31a00a0781a7a433
hikkiabc <hikkiabc@gmail.com>
2020-09-20 07:58:00 -0400

    init

---
+++ gl-product/src/test/java/com/glmall/glproduct/GlProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glmall.glproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
